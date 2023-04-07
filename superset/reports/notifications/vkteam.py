76d060a5b2a33e7f99cd22ff8a2d728e83c8de46
Semen Rozhkov <sprozhkov@yanao.ru>
2023-04-07 10:34:13 +0500

    Отправка в vkteam, работает

--- superset/reports/notifications/vkteam.py
+++ superset/reports/notifications/vkteam.py
@@ -1,27 +1,28 @@
 import json
 import logging
+import requests
 
 from superset import app
 from superset.models.reports import ReportRecipientType
 from superset.reports.notifications.base import BaseNotification
 from superset.reports.notifications.exceptions import NotificationError
 from superset.utils.decorators import statsd_gauge
-import requests
 
 logger = logging.getLogger(__name__)
 
 
-class VkteamNotification(BaseNotification): # pylint: disable=too-few-public-methods
+class VkteamNotification(BaseNotification):  # pylint: disable=too-few-public-methods
     type = ReportRecipientType.VKTEAM
 
     def _get_token(self) -> str:
-        return app.config('VKTEAM_API_TOKEN')
+        return app.config['VKTEAM_API_TOKEN']
 
     def _get_url(self) -> str:
-        return app.config('VKTEAM_URL')
+        return app.config['VKTEAM_URL']
 
     def _get_to(self) -> str:
         return json.loads(self._recipient.recipient_config_json)["target"]
+
 
     @statsd_gauge("reports.vkteam.send")
     def send(self) -> None:
@@ -30,14 +31,17 @@ class VkteamNotification(BaseNotification): # pylint: disable=too-few-public-met
         to = self._get_to()
 
         if self._content.screenshots:
-            text = self._content.text.replace('_', '\_')
+            if self._content.text:
+                text = self._content.text #.replace('_', '\_')
+            else:
+                text = self._content.name #.replace('_', '\_')
         try:
-            requests.post(f'{url}messages/sendText', params={
+            requests.post(f'{url}messages/sendFile', params={
                 'token': token,
                 'chatId': to,
-                'caption': text,
-                'file': self._content.screenshots[0].decode('utf-8')
-            })
+                'caption': text
+            }, files={"file": self._content.screenshots[0]})
+
             logger.info('Report sent to vkteam')
         except Exception as ex:
             raise NotificationError(ex) from ex
